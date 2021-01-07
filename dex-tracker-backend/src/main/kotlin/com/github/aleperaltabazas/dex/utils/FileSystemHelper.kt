91bcfe4d85ccc1dcc29bbb3c69274a93d825d7a6
Alejandro <aleperaltabazas@hotmail.com>
2021-01-07 20:07:14 -0300

    adding FileSystemHelper

---
+++ dex-tracker-backend/src/main/kotlin/com/github/aleperaltabazas/dex/utils/FileSystemHelper.kt
@@ -0,0 +1,31 @@
+package com.github.aleperaltabazas.dex.utils
+
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Paths
+
+open class FileSystemHelper {
+    open fun createDirectoryIfItDoesNotExist(path: String) {
+        val file = File(path)
+
+        when {
+            file.exists() && file.isDirectory -> throw IllegalArgumentException("File $path already exists as a file")
+            !file.exists() -> file.mkdir()
+        }
+    }
+
+    open fun createFile(content: String, path: String) = createFileFromInputStream(
+        filePath = path,
+        input = content.byteInputStream()
+    )
+
+    open fun deleteFile(file: File) = Files.delete(file.toPath())
+
+    private fun createFileFromInputStream(filePath: String, input: InputStream): File {
+        val pathToFile = Paths.get(filePath)
+        Files.copy(input, pathToFile)
+
+        return pathToFile.toFile()
+    }
+}
