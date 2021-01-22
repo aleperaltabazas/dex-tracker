08d05b3dd6d91d294b639ffcdc8b9e7e6b849991
Alejandro <aleperaltabazas@hotmail.com>
2021-01-22 19:11:57 -0300

    adding empty MyDexPage

---
+++ dex-tracker-frontend/src/views/MyDexPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+type MyDexPageProps = {};
+
+const MyDexPage = (props: MyDexPageProps) => {
+  return <div></div>;
+};
+
+export default hot(module)(MyDexPage);
