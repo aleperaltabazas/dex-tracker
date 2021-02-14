f4417f9eb602ca3b8112a9387dd43e1a41b1f762
Alejandro <aleperaltabazas@hotmail.com>
2021-02-14 12:18:41 -0300

    delete local/remote rendering for dex

--- dex-tracker-frontend/src/views/DexPage.tsx
+++ dex-tracker-frontend/src/views/DexPage.tsx
@@ -6,12 +6,25 @@ import { connect } from "react-redux";
 import { PokedexState } from "../store/pokedex";
 import Loader from "../components/Loader";
 import { SessionState } from "../store/session";
-import Local from "./DexPage/Local";
-import Remote from "./DexPage/Remote";
+import { Container, makeStyles } from "@material-ui/core";
+import classNames from "classnames";
+import { UserDex } from "../types/user";
+import Dex from "../components/Dex";
 
 type MatchParams = {
   id: string;
 };
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: "white",
+  },
+  noOverflow: {
+    [theme.breakpoints.down("md")]: {
+      overflowX: "hidden",
+    },
+  },
+}));
 
 interface DexPageProps extends RouteComponentProps<MatchParams> {
   gamePokedex: PokedexState;
@@ -19,7 +32,8 @@ interface DexPageProps extends RouteComponentProps<MatchParams> {
 }
 
 const DexPage = (props: DexPageProps) => {
-  if (!props.gamePokedex.loaded) {
+  const classes = useStyles();
+  if (!props.gamePokedex.loaded || props.session.type == "UNINITIALIZED") {
     return (
       <div className="h-100 w-100 center">
         <Loader />
@@ -27,25 +41,40 @@ const DexPage = (props: DexPageProps) => {
     );
   }
 
-  switch (props.session.type) {
-    case "NOT_LOGGED_IN":
-      return (
-        <Local
-          id={props.match.params.id}
-          gamePokedex={props.gamePokedex.pokedex}
-        />
-      );
-    case "LOGGED_IN": {
-      return (
-        <Remote
-          id={props.match.params.id}
-          gamePokedex={props.gamePokedex.pokedex}
-        />
-      );
-    }
-    default:
-      return <Redirect to="/" />;
+  if (props.session.type == "ERROR") {
+    return <Redirect to="/" />;
   }
+
+  let dex: UserDex | undefined;
+
+  if (props.session.type == "LOGGED_IN") {
+    dex = props.session.user.pokedex.find(
+      (d) => d.userDexId == props.match.params.id
+    );
+  } else {
+    dex = props.session.localDex.find(
+      (d) => d.userDexId == props.match.params.id
+    );
+  }
+
+  if (!dex) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <Container className={classNames(classes.noOverflow, "center")}>
+      <div className={classNames(classes.container, "mt-3 mt-md-5")}>
+        <Dex
+          dex={dex}
+          gamePokedex={
+            props.gamePokedex.pokedex.find(
+              (gp) => gp.game.title == dex!.game.title
+            )!
+          }
+        />
+      </div>
+    </Container>
+  );
 };
 
 const mapStateToProps = (root: RootState) => {
